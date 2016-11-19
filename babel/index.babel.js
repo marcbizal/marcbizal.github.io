5f50d36d73d806ae520013b5a1595406d491502a
Marcus Bizal <hello@marcbizal.com>
2016-11-18 23:55:41 -0500

    Small fixes for responsive design

--- babel/index.babel.js
+++ babel/index.babel.js
@@ -1,7 +1,9 @@
+var particles = ($(window).width() * $(window).height()) / 20000;
+
 particlesJS("particles-js", {
   "particles": {
     "number": {
-      "value": 64,
+      "value": particles,
       "density": {
         "enable": false,
         "value_area": 800
@@ -15,14 +17,6 @@ particlesJS("particles-js", {
       "stroke": {
         "width": 0,
         "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
       }
     },
     "opacity": {
@@ -64,45 +58,6 @@ particlesJS("particles-js", {
         "enable": false,
         "rotateX": 600,
         "rotateY": 1200
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": false,
-        "mode": "repulse"
-      },
-      "onclick": {
-        "enable": false,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 400,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
       }
     }
   },
